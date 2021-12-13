b1f261e935b530cd310e9e3d6108e51f7d8fd87d
leephan2k1 <pttl2k1@gmail.com>
2021-12-13 15:03:46 +0700

    template home page

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const ap = new APlayer({
+  container: document.getElementById("aplayer"),
+  audio: [
+    {
+      name: "name",
+      artist: "artist",
+      listFolded: false,
+      listMaxHeight: 90,
+      mini: false,
+      url: "/public/music/song1.mp3",
+      cover:
+        "https://photo-resize-zmp3.zadn.vn/w240_r1x1_webp/cover/3/a/1/a/3a1aad7f3f927dece72b0d57642f5c8a.jpg",
+    },
+  ],
+});
